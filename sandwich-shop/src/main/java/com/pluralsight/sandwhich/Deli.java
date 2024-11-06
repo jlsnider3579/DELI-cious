4e60cf48b22a09d2892fadb0f58d0f603d496746
Jontez Snider <jlsnider3579@gmail.com>
2024-11-06 15:59:36 -0500

    Initial code commit

---
+++ sandwich-shop/src/main/java/com/pluralsight/sandwhich/Deli.java
@@ -0,0 +1,57 @@
+package com.pluralsight.sandwhich;
+
+import java.awt.*;
+import java.util.List;
+
+public class Deli {
+    // List of orders currently being processed
+    private List<Order> currentOrders;
+
+    // List of sandwiches offered at the deli
+    private List<Menu> menu;
+
+    // List of ingredients in the inventory (simplified as List<String>)
+    private List<String> inventory;
+
+    // Constructor
+    public Deli(List<Order> currentOrders, List<Menu> menu, List<String> inventory) {
+        this.currentOrders = currentOrders;
+        this.menu = menu;
+        this.inventory = inventory;
+    }
+
+    // Getters and setters
+    public List<Order> getCurrentOrders() {
+        return currentOrders;
+    }
+
+    public void setCurrentOrders(List<Order> currentOrders) {
+        this.currentOrders = currentOrders;
+    }
+
+    public List<Menu> getMenu() {
+        return menu;
+    }
+
+    public void setMenu(List<Menu> menu) {
+        this.menu = menu;
+    }
+
+    public List<String> getInventory() {
+        return inventory;
+    }
+
+    public void setInventory(List<String> inventory) {
+        this.inventory = inventory;
+    }
+
+    @Override
+    public String toString() {
+        return "Deli{" +
+                "currentOrders=" + currentOrders +
+                ", menu=" + menu +
+                ", inventory=" + inventory +
+                '}';
+    }
+}
+
---
+++ sandwich-shop/src/main/java/com/pluralsight/sandwhich/Program.java
@@ -0,0 +1,4 @@
+package com.pluralsight.sandwhich;
+
+public class Program {
+}
