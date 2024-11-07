4288630a86633457af91030bac71f91d0402eaea
Jontez Snider <jlsnider3579@gmail.com>
2024-11-06 19:49:57 -0500

    Created a customer class with with some hard coded methods to add, view, remove, or clear an order. Methods do not have function yet.

---
+++ sandwich-shop/src/main/java/com/pluralsight/sandwhich/Customer.java
@@ -0,0 +1,59 @@
+package com.pluralsight.sandwhich;
+
+import java.util.List;
+
+public class Customer {
+
+    // The name of the customer
+    private String customerName;
+
+    // The customer's current order
+    private Order customerOrder;
+
+    // Constructor
+    public Customer(String customerName, Order customerOrder) {
+        this.customerName = customerName;
+        this.customerOrder = customerOrder;
+    }
+
+    // Getters and setters
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public Order getCustomerOrder() {
+        return customerOrder;
+    }
+
+    public void setCustomerOrder(Order customerOrder) {
+        this.customerOrder = customerOrder;
+    }
+
+    // Method for adding an item to the customer's order
+    public void addItemToOrder() {
+    }
+
+    // Method to view the current order details
+    public void viewOrder() {
+    }
+
+    // Method to remove a specific item
+    //public boolean removeOrder(){
+    // }
+
+    // Method to clear the customer's current order
+    public void clearOrder() {
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "customerName='" + customerName + '\'' +
+                ", customerOrder=" + customerOrder +
+                '}';
+    }
+}
