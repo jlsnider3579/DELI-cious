7e4e5b5613820e9df5bed07ac2d190211d7e5326
Jontez Snider <jlsnider3579@gmail.com>
2024-11-13 11:43:10 -0500

    Moved my abstract premimum class that extends from topping has it variables and constructor that intializes the objects with the the get price method that will be used in its subclasses

---
+++ sandwich-shop/src/main/java/com/pluralsight/sandwich/PremiumTopping.java
@@ -0,0 +1,20 @@
+package com.pluralsight.sandwich;
+
+// The PremiumTopping class is an abstract subclass of the Topping class.
+// It represents a premium topping that can be added to a sandwich.
+public abstract class PremiumTopping extends Topping {
+    protected double price;
+
+    // Constructor for creating a PremiumTopping object with a specific size and whether it's extra.
+    // The price of the topping will be set according to the topping type
+    public PremiumTopping(SandwichSize size, boolean hasExtra) {
+        super(size, hasExtra);
+        this.price = price;
+    }
+
+
+    @Override
+    public double getPrice() {
+        return price;
+    }
+}
